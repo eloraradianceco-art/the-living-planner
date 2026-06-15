0bc3a0211bd2bc749bb2866d15f564636fceaa38
eloraradianceco-art <eloraradiance.co@gmail.com>
2026-06-15 10:13:15 -0500

    Fix: render ResetPassword on /reset-password so password-reset links work (was only rendering App)

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App_Planner'
+import ResetPassword from './ResetPassword'
 import './styles.css'
+
+const isReset = window.location.pathname === '/reset-password'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {isReset ? <ResetPassword /> : <App />}
     </BrowserRouter>
   </React.StrictMode>
 )
